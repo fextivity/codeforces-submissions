72831a0c1acfea8fc1efae1eb9d1cc15f44878d5
thenymphsofdelphi <trxbach135@gmail.com>
2017-11-14 06:04:00 +0700

    Add solution for problem `B - Taxi` Link: https://codeforces.com/contest/158/problem/B Tags: *special problem, greedy, implementation, *1100 Ref: https://codeforces.com/contest/158/submission/32295623

--- codeforces/158/B.cpp
+++ codeforces/158/B.cpp
@@ -1,18 +1,33 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#define min(a, b) ((a < b) ? (a) : (b))
 using namespace std;
 
-int a[5];
-
 int main(){
-	int n, t;
-	cin >> n;
-	for (int i = 0; i < n; i++){
-		cin >> t;
-		a[t]++;
-	}
-	a[1] -= a[3];
-	if (a[1] < 0){
-	    a[1] = 0;
-	}
-	cout << a[3] + a[4] + (a[2] + (a[1] + 1) / 2 + 1) / 2;
+    long n, i, tol = 0;
+    cin >> n;
+    int a[n], b[4] = {0, 0, 0, 0}, temp;
+    for (i = 0; i < n; i++){
+        cin >> a[i];
+        b[a[i] - 1]++;
+    }
+    tol += b[3];
+    temp = min(b[0], b[2]);
+    tol += temp;
+    b[0] -= temp;
+    b[2] -= temp;
+    if (b[0] == 0) tol += b[2];
+    tol += b[1] / 2;
+    b[1] = b[1] % 2;
+    tol += b[0] / 4;
+    if (b[0] != 0){
+        b[0] = b[0] % 4;
+        if (b[1] == 1){
+            tol += 1;
+            b[1] -= 1;
+            b[0] -= 2;
+        }
+        if (b[0] > 0) tol += 1;
+    }
+    tol += b[1];
+    cout << tol;
 }
