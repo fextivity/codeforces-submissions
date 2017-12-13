ffee33ab5dc4824fb982f06b6cab45ba593af75f
thenymphsofdelphi <trxbach135@gmail.com>
2017-12-14 05:51:00 +0700

    Add solution for problem `A - Beautiful Matrix` Link: https://codeforces.com/contest/263/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/263/submission/33226879

--- codeforces/263/A.cpp
+++ codeforces/263/A.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main(){
-    int i, j, iii, jjj, ans;
+    int i, j, ii, jj, ans;
     int a[5][5];
     for (i = 0; i < 5; i++){
         for (j = 0; j < 5; j++){
             cin >> a[i][j];
             if (a[i][j] == 1){
-                iii = i;
-                jjj = j;
+                ii = i;
+                jj = j;
             }
         }
     }
-    ans = abs(iii - 2) + abs(jjj - 2);
+    ans = abs(ii - 2) + abs(jj - 2);
     cout << ans;
 }
