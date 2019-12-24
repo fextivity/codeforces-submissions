b782df14494bc1937f2d2ea6f2331c9274e0306c
thenymphsofdelphi <trxbach135@gmail.com>
2019-12-24 09:50:00 +0700

    Add solution for problem `A - Power Consumption Calculation` Link: https://codeforces.com/contest/10/problem/A Tags: implementation, *900 Ref: https://codeforces.com/contest/10/submission/67499105

--- codeforces/10/A.cpp
+++ codeforces/10/A.cpp
@@ -1,32 +1,139 @@
-#include <iostream>
-#include <cstdio>
-
+#include<bits/stdc++.h>
 using namespace std;
-const int MAXN = 105;
 
-int main()
-{
-    #ifndef ONLINE_JUDGE
-    freopen("calcpower.inp","r",stdin);
-    /reopen("calcpower.out","w",stdout);
-    #endif // ONLINE_JUDGE
-    int n, p1, p2, p3, t1, t2, time1 = 0, time2=0, time3 = 0;
-    int l[MAXN], r[MAXN];
-    cin >> n >> p1 >> p2 >> p3 >> t1 >> t2;
-    for(int i=0; i < n ; i++) {
-        cin >> l[i] >> r[i];
-        time1 +=r[i]-l[i];
-    }
-    for(int i = 1; i < n; i++){
-        int t = l[i] - r[i-1];
-        time1 += (t < t1? t : t1);
-        t -= t1;
-        if (t <= 0) continue;
-        time2 += (t < t2? t : t2);
-        t -= t2;
-        if (t <= 0) continue;
-        time3 += t;
-    }
-    cout << time1*p1 + time2*p2 + time3*p3 << endl;
-    return 0;
+// Optimization
+//#pragma GCC optimize("O3")
+#define endl '\n'
+
+// Shortcut
+
+#define int long long
+#define eb emplace_back
+#define pb push_back
+#define pob pop_back
+#define mp make_pair
+#define upb upper_bound
+#define lwb lower_bound
+#define fi first
+#define se second
+#define For(i, l, r) for (int i = l; i < r; i++)
+#define ForE(i, l, r) for (int i = l; i <= r; i++)
+#define Ford(i, r, l) for (int i = r; i > l; i--)
+#define FordE(i, r, l) for (int i = r; i >= l; i--)
+#define Fora(i, a) for (auto i : a)
+
+// I/O & Debug
+
+#define PrintV(a) for (int iiii = 0; iiii < a.size(); iiii++) cout << a[iiii] << ' ';
+#define PrintVl(a) for (int iiii = 0; iiii < a.size(); iiii++) cout << a[iiii] << endl;
+#define PrintA(a, n) for (int iiii = 0; iiii < n; iiii++) cout << a[iiii] << ' ';
+#define PrintAl(a, n) for (int iiii = 0; iiii < n; iiii++) cout << a[iiii] << endl;
+#define Ptest(x) return cout << x, 0;
+#define gl(s) getline(cin, s);
+#define setpre(x) fixed << setprecision(x)
+/*
+#define debug(args...){ string _sDEB = #args; replace(_sDEB.begin(), _sDEB.end(), ',', ' '); stringstream _ssDEB(_sDEB); istream_iterator<string> _itDEB(_ssDEB); DEB(_itDEB, args); }
+void DEB(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void DEB(istream_iterator<string> it, T a, Args... args){
+    cout << *it << " = " << a << endl;
+    DEB(++it, args...);
 }
+*/
+
+// Functions
+
+//#define isvowel(a) (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u')
+#define bend(a) a.begin(), a.end()
+#define rbend(a) a.rbegin(), a.rend()
+#define mset(a) memset(a, 0, sizeof(a))
+#define mset1(a) memset(a, 1, sizeof(a))
+#define msetn1(a) memset(a, -1, sizeof(a))
+#define msetinf(a) memset(a, 0x3f, sizeof(a))
+#define gcd __gcd
+#define __builtin_popcount __builtin_popcountll
+//mt19937 rando(chrono::steady_clock::now().time_since_epoch().count());
+
+// Data Structure
+
+#define pque priority_queue
+#define mts multiset
+#define y0 _y0_
+#define y1 _y1_
+#define div divi
+typedef long long ll;
+typedef long double ld;
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef vector <ld> vld;
+typedef vector <string> vs;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef vector <vi > vvi;
+typedef vector <vll > vvll;
+typedef vector <pii > vpii;
+typedef vector <pll > vpll;
+
+const int N = 1440 + 5, mod = 1e9 + 7, mod1 = 998242353, mod2 = 1e9 + 9, inf = 1e9 + 7;
+const ll infll = 1e18 + 7;
+
+int n, p1, p2, p3, t1, t2, l, r, ans;
+bool a[N];
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    //freopen("POWER.inp", "r", stdin);
+    //freopen("POWER.out", "w", stdout);
+	cin >> n >> p1 >> p2 >> p3 >> t1 >> t2;
+	t2 += t1;
+	ForE(i, 1, n){
+		int x, y;
+		cin >> x >> y;
+		if (i == 1) l = x + 1;
+		if (i == n) r = y;
+		ForE(j, x + 1, y){
+			a[j] = 1;
+		}
+	}
+	int tmp = 0;
+	ForE(i, l, r){
+		if (!a[i]){
+			tmp++;
+		}
+		else{
+			tmp = 0;
+		}
+		if (tmp <= t1){
+			ans += p1;
+		}
+		else if (tmp <= t2){
+			ans += p2;
+		}
+		else{
+			ans += p3;
+		}
+	}
+	cout << ans;
+}
+
+/*
+==================================+
+INPUT:                            |
+------------------------------    |
+1 3 2 1 5 10
+0 10
+------------------------------    |
+2 8 4 2 5 10
+20 30
+50 100
+------------------------------    |
+==================================+
+OUTPUT:                           |
+------------------------------    |
+30
+------------------------------    |
+570
+------------------------------    |
+==================================+
+*/
